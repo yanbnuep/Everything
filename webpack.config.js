7d7989b91169d0e28620db7f3c76cfb0fd483be6
yanbnuep <yanbnuep@gmail.com>
2017-07-18 21:41:25 +0800

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,14 @@ var path = require('path');
 var loaders = require('./webpack.loaders');
 
 module.exports = {
-	
+	entry: {
+		app:['./index.jsx']
+	},
+	module: {
+		loaders: loaders
+	},
+	output: {
+		path: path.resolve(__dirname,"bundle"),
+		filename: "bundle.js"
+	}
 }
--- webpack.loaders.js
+++ webpack.loaders.js
@@ -1,6 +1,10 @@
 module.exports = [
 	{
 		test: /\.jsx?$/,
-		loader: ['babel'],
+		exclude: /(node_modules|bower_components)/,
+		loader: 'babel-loader',
+		query: {
+			presets: ['es2015', 'react']
+		}
 	}
 ]
